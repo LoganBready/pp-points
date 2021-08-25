79ebd4d0f0c546da68218d5270ffc065c1e58f14
mroseweir <roseweirmichael@gmail.com>
2021-08-25 14:33:55 -0500

    test

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   // app.get("*", (req, res) => {
   //   req.sendFile(path.resolve(__dirname, "build", "index.html"));
   // });
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
