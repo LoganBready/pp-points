ead4a1a27c53954d8a6b4384ffce24ffc2eb92d2
Wesley McGhee <wesmcghee6372@gmail.com>
2021-08-25 12:42:18 -0500

    fixed add student

--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/footer";
 
-const baseURL = `http://localhost:4500/api/points`;
+const baseURL = `/api/points`;
 
 function AddStudent() {
   const [name, setName] = useState("");
